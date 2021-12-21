8e42ca26c3a12080e9263ef76d97e1128831b94d
Vera <vera.uzunova@voiapp.io>
2021-12-21 15:36:45 +0100

    New tests added

--- src/TodoList.test.tsx
+++ src/TodoList.test.tsx
@@ -1,16 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { storeInput } from './storage';
+import { TodoItemObj } from './TodoItem';
 import { TodoList } from './TodoList';
 
-jest.mock('./storage', () => ({
-    storeInput: {
-        get: jest.fn(),
-        add: jest.fn()
-    },
+jest.mock('./storage', () => ({storeInput: {
+    get: jest.fn(),
+    add: jest.fn()
+    }
 }))
 
-/*beforeEach(() => {
-    localStorage.clear()
-})*/
+let onChangeExistingTodoItem: (newValue:string, id:string) => void
+jest.mock('./TodoItem', () => ({
+    TodoItem: (props:{
+        onChangeExistingTodoItem:any
+    }) => {
+        onChangeExistingTodoItem = props.onChangeExistingTodoItem
+        return <div></div>
+    },
+}))
 
 test('Should disable new todo button when input field is empty', () => {
     render(<TodoList />)
@@ -53,4 +60,21 @@ test('Should not create todo item when the Enter key is pressed and the input fi
     })
     const containerEl = screen.getByTestId('todo-list-container')
     expect(containerEl.children).toHaveLength(0)
+})
+
+test('Should not update existing todo item when new value is empty', () => {
+    render(<TodoList />)
+    onChangeExistingTodoItem(" ", "123")
+    expect(storeInput.add).toBeCalledTimes(0)
+})
+
+test('Should update existing todo item when new value is not empty', () => {
+    const todoItems: TodoItemObj[] = [
+        {isDone:false,isEditing:false,id:"123",value:"New-new"},
+        {isDone:false,isEditing:false,id:"1234",value:"New-new-"}
+    ]
+    ;(storeInput.get as jest.Mock).mockReturnValue(todoItems)
+    render(<TodoList />)
+    onChangeExistingTodoItem("New thing", "123")
+    expect(storeInput.add).toBeCalledTimes(1)
 })
